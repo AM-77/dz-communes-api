c1cb3b2966f18aca06ae6b8f7e606d8c4d97fbeb
AM-77 <amine.griche77@protonmail.ch>
2020-09-07 11:10:55 -0500

    add build script

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var app_1 = require("./app");
-var PORT = process.env.PORT || 4800;
-var server = app_1["default"].listen(PORT, function () { return console.log("[+] The dz-api is running on port " + PORT); });
-exports["default"] = server;
